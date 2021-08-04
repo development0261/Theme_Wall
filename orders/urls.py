44e734dcb83aeda984fdaef387738392c88e741b
Your Name <development0261@gmail.com>
2021-08-04 14:02:44 +0530

    Add to cart, cart drawer

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns = [
     path('',ordersIndex,name="ordersIndex"),
     path('cart/',cart,name="cart"),
+    path('checkout/',checkout,name="checkout"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def ordersIndex(request):
     pass
 
 def cart(request):
-    return render(request,'products/cartPage.html')
+    if request.user.is_authenticated:
+        return render(request,'products/cartPage.html')
+    else:
+        return redirect('login')
+
+def checkout(request):
+    if request.user.is_authenticated:
+        return render(request,'products/checkout.html')
+    else:
+        return redirect('login')
