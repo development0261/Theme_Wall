b590c11f63e4fe502a61ae2775e93b28c8222177
Your Name <development0261@gmail.com>
2022-01-05 14:05:55 +0530

    otp

--- themenswall/asgi.py
+++ themenswall/asgi.py
@@ -8,18 +8,26 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from django.core.asgi import get_asgi_application
-import feed
-from channels.auth import AuthMiddlewareStack
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'themenswall.settings')
+import django
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            feed.routing.websocket_urlpatterns
-        )
-    ),
-})
+from channels.routing import get_default_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'themenswall.settings')
+django.setup()
+application = get_default_application()
+
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
+# import feed
+# from channels.auth import AuthMiddlewareStack
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'themenswall.settings')
+
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             feed.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
